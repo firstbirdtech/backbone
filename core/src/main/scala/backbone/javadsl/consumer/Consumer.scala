c5641a6e32b78a476e17b058f19bb7214e490732
Georgi Lichev <georgi.i.lichev@gmail.com>
2017-03-09 11:10:02 +0100

    temp

--- core/src/main/scala/backbone/javadsl/consumer/Consumer.scala
+++ core/src/main/scala/backbone/javadsl/consumer/Consumer.scala
@@ -1,10 +1,33 @@
 package backbone.javadsl.consumer
 
+import java.util.concurrent.{CompletionStage, Future}
+import java.util.function.{Function => JFunction}
+
+import akka.Done
 import akka.actor.ActorSystem
+import backbone.consumer.Consumer.{Settings => SSettings}
+import backbone.consumer.{Consumer => SConsumer}
+import backbone.format.Format
+import backbone.scaladsl.Backbone.ProcessingResult
 import com.amazonaws.services.sqs.AmazonSQSAsyncClient
 
-class Consumer(settings:Settings,system:ActorSystem,sqs:AmazonSQSAsyncClient) {
+import scala.collection.JavaConverters._
+import scala.compat.java8.{FunctionConverters, FutureConverters, OptionConverters}
 
+class Consumer(settings:Settings,system:ActorSystem,sqs:AmazonSQSAsyncClient) {
+  val asScalaSettings = SSettings(
+    settings.queueUrl,
+    settings.event.asScala.toList,
+    settings.parallelism,
+    OptionConverters.toScala(settings.limitation)
+  )
+  val asScala = new SConsumer(asScalaSettings)(system,sqs)
+
+  def consumeAsync[T](fo:Format[T],f: JFunction[T, CompletionStage[ProcessingResult]]): Future[Done] = {
+    val asScalaFunction: (T) => CompletionStage[ProcessingResult] = FunctionConverters.asScalaFromFunction(f)
+    val asScalaFuture                                             = asScalaFunction.andThen(a => FutureConverters.toScala(a))
+    FutureConverters.toJava(asScala.consumeAsync[T](asScalaFuture)(fo)).toCompletableFuture
+  }
 
 }
 
