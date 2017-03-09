6c6cef8ed3a7b3a564b5056dfef78246c4773a0b
Georgi Lichev <georgi.i.lichev@gmail.com>
2017-03-09 11:31:37 +0100

    remove consumer

--- core/src/main/scala/backbone/javadsl/consumer/Settings.scala
+++
@@ -1,6 +0,0 @@
-package backbone.javadsl.consumer
-
-import java.util.{List => JList, Optional => JOption}
-import backbone.consumer.Limitation
-
-case class Settings(queueUrl: String, event: JList[String], parallelism: Integer, limitation: JOption[Limitation]) {}
