fdfc81b240c2df344beeb608f7d944ee4d6dbb41
Scala Steward <me@scala-steward.org>
2020-09-10 09:58:50 +0200

    Update play-json to 2.9.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   private[this] val junitVersion                 = "4.13"
   private[this] val logbackVersion               = "1.2.3"
   private[this] val mockitoScalaVersion          = "1.14.8"
-  private[this] val playJsonVersion              = "2.9.0"
+  private[this] val playJsonVersion              = "2.9.1"
   private[this] val scalaTestVersion             = "3.2.0"
   private[this] val scalaCollectionCompatVersion = "2.1.6"
   private[this] val slf4jVersion                 = "1.7.30"
