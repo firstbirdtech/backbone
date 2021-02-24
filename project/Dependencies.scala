a705287c25360fb904192323d1dd4458852cb389
Scala Steward <me@scala-steward.org>
2021-02-24 16:22:09 +0100

    Update mockito-scala to 1.16.29

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   private[this] val java8CompatVersion           = "0.9.1"
   private[this] val junitVersion                 = "4.13.2"
   private[this] val logbackVersion               = "1.2.3"
-  private[this] val mockitoScalaVersion          = "1.16.25"
+  private[this] val mockitoScalaVersion          = "1.16.29"
   private[this] val playJsonVersion              = "2.9.2"
   private[this] val scalaTestVersion             = "3.2.4"
   private[this] val scalaCollectionCompatVersion = "2.4.2"
