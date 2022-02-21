577a88b71737810e0248762cc23e702119abae9c
Scala Steward <me@scala-steward.org>
2022-02-21 14:22:07 +0100

    Update elasticmq-rest-sqs to 1.3.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -51,7 +51,7 @@ object Dependencies {
     "com.typesafe.akka"     %% "akka-slf4j"         % akkaVersion,
     "com.typesafe.akka"     %% "akka-testkit"       % akkaVersion,
     "io.circe"              %% "circe-parser"       % circeVersion,
-    "org.elasticmq"         %% "elasticmq-rest-sqs" % "1.3.4",
+    "org.elasticmq"         %% "elasticmq-rest-sqs" % "1.3.5",
     "org.mockito"           %% "mockito-scala"      % "1.17.5",
     "org.scalatest"         %% "scalatest"          % scalaTestVersion,
     "software.amazon.awssdk" % "sqs"                % "2.17.133"
