a0502eb90ab43f6a34df4ae48336c12c98dfeef7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-03-06 02:28:08 +0100

    Update logback-classic to 1.2.11 (#311)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   private[this] val alpakkaVersion        = "3.0.4"
   private[this] val akkaVersion           = "2.6.18"
   private[this] val circeVersion          = "0.14.1"
-  private[this] val logbackClassicVersion = "1.2.10"
+  private[this] val logbackClassicVersion = "1.2.11"
   private[this] val scalaTestVersion      = "3.2.10"
   private[this] val slf4jVersion          = "1.7.36"
 
