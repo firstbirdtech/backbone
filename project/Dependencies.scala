48f3be061623391f83a01c8a6e4b458cf95ff006
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-31 16:19:35 +0200

    Update scalatest to 3.2.13 (#369)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   private[this] val akkaVersion           = "2.6.19"
   private[this] val circeVersion          = "0.14.2"
   private[this] val logbackClassicVersion = "1.2.11"
-  private[this] val scalaTestVersion      = "3.2.12"
+  private[this] val scalaTestVersion      = "3.2.13"
   private[this] val slf4jVersion          = "1.7.36"
 
   private[this] val testDependencies: Seq[ModuleID] = Seq(
