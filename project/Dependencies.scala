e7bfa0a756b08a97dec06242454405de389d289a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-02 16:14:04 +0200

    Update gson to 2.9.1 (#371)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies {
   ) ++ testDependencies
 
   val jsonGson: Seq[ModuleID] = Seq(
-    "com.google.code.gson" % "gson" % "2.9.0"
+    "com.google.code.gson" % "gson" % "2.9.1"
   ) ++ testDependencies
 
   val testutils: Seq[ModuleID] = Seq(
