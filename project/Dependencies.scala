fcc19c3ccbe8c00b39615cf191d1404645e379d6
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-04 16:09:06 +0200

    Update mockito-scala to 1.17.12 (#373)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -52,7 +52,7 @@ object Dependencies {
     "com.typesafe.akka"     %% "akka-testkit"       % akkaVersion,
     "io.circe"              %% "circe-parser"       % circeVersion,
     "org.elasticmq"         %% "elasticmq-rest-sqs" % "1.3.9",
-    "org.mockito"           %% "mockito-scala"      % "1.17.7",
+    "org.mockito"           %% "mockito-scala"      % "1.17.12",
     "org.scalatest"         %% "scalatest"          % scalaTestVersion,
     "software.amazon.awssdk" % "sqs"                % "2.17.244"
   )
