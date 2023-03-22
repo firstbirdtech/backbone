fbac72d6859f0882f9c4fde2c33defb8fef2303e
Scala Steward <scala_steward@virtuslab.com>
2023-03-22 15:32:38 +0000

    Update mockito-scala to 1.17.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -52,7 +52,7 @@ object Dependencies {
     "com.typesafe.akka"     %% "akka-testkit"       % akkaVersion,
     "io.circe"              %% "circe-parser"       % circeVersion,
     "org.elasticmq"         %% "elasticmq-rest-sqs" % "1.3.14",
-    "org.mockito"           %% "mockito-scala"      % "1.17.12",
+    "org.mockito"           %% "mockito-scala"      % "1.17.14",
     "org.scalatest"         %% "scalatest"          % scalaTestVersion,
     "software.amazon.awssdk" % "sqs"                % "2.20.27"
   )
