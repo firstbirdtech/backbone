b2d335029f0ef72216a97877d7949163b9eec3af
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-07-14 20:05:04 +0200

    Update sqs to 2.20.103 (#551)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,7 +54,7 @@ object Dependencies {
     "org.elasticmq"         %% "elasticmq-rest-sqs" % "1.4.2",
     "org.mockito"           %% "mockito-scala"      % "1.17.14",
     "org.scalatest"         %% "scalatest"          % scalaTestVersion,
-    "software.amazon.awssdk" % "sqs"                % "2.20.101"
+    "software.amazon.awssdk" % "sqs"                % "2.20.103"
   )
 
   val integrationtest: Seq[ModuleID] = Seq(
