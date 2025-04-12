260868261d330c2871029746c67a51ae46d3390c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-04-12 17:34:47 +0200

    Update gson to 2.13.0 (#915)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -47,7 +47,7 @@ object Dependencies {
   ) ++ testDependencies
 
   val jsonGson: Seq[ModuleID] = Seq(
-    "com.google.code.gson" % "gson" % "2.12.1"
+    "com.google.code.gson" % "gson" % "2.13.0"
   ) ++ testDependencies
 
   val testutils: Seq[ModuleID] = Seq(
